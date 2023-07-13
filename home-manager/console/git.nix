40da95d3cff357b20f0528da1d5c9f4799a2e40d
Zach Bombay <zacharybombay@gmail.com>
2023-07-12 20:30:43 -0400

    removed unstable option in git.nix

--- home-manager/console/git.nix
+++ home-manager/console/git.nix
@@ -2,7 +2,8 @@
   programs = {
     gh = {
       enable = true;
-      extensions = with pkgs.unstable; [ gh-markdown-preview ];
+      extensions = with pkgs; [ gh-markdown-preview ];
+      # extensions = with pkgs.unstable; [ gh-markdown-preview ];
       settings = {
         git_protocol = "ssh";
         prompt = "enabled";
