cb9f851cdb04c6e245e0e3942285af7a06b05a61
Zach Bombay <zacharybombay@gmail.com>
2023-07-12 20:29:49 -0400

    add .overlay to emacs-overlay

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -33,7 +33,7 @@ in {
       # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
-      outputs.emacs-overlay
+      inputs.emacs-overlay.overlay
       # outputs.overlays.unstable-packages
 
       # You can also add overlays exported from other flakes:
