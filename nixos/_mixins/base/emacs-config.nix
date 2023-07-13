f76bffe90e2737084da517227ee39ca6cf65ea20
Zach Bombay <zacharybombay@gmail.com>
2023-07-12 21:54:51 -0400

    commentted out bit in emacs-config.nix

--- nixos/_mixins/base/emacs-config.nix
+++ nixos/_mixins/base/emacs-config.nix
@@ -2,26 +2,18 @@
 
 {
   imports = [
-    # inputs.emacs-overlay
+
   ];
   services.emacs = {
     enable = true;
     package = pkgs.emacs-unstable;
-    # package = pkgs.emacsUnstable; # replace with emacs-gtk, emacsUnstable, or a version provided by the community overlay if desired.
     defaultEditor = true;
   };
-
-  # nixpkgs.overlays = [
-  #   (import (builtins.fetchTarball {
-  #     url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-  #   }))
-  # ];
 
   environment.systemPackages = [
     # applications needed for the emacs packages to hook into
     pkgs.python311Full
     pkgs.python311Packages.jedi
-    # pkgs.python3Full.jedi
     pkgs.python311Packages.black
     pkgs.python311Packages.jedi-language-server
     pkgs.texlive.combined.scheme-full
@@ -35,33 +27,32 @@
       alwaysTangle = true;
       defaultInitFile = true;
 
-    #   # Optionally provide extra packages not in the configuration file.
-      extraEmacsPackages = epkgs: [
-        epkgs.auto-compile
-        epkgs.blacken
-        epkgs.company
-        epkgs.dashboard
-        epkgs.expand-region
-        epkgs.flycheck
-        epkgs.latex-preview-pane
-        epkgs.lsp-jedi
-        epkgs.lsp-ui
-        epkgs.minions
-        epkgs.nix-mode
-        epkgs.nix-modeline
-        epkgs.org-books
-        epkgs.org-chef
-        epkgs.popup-kill-ring
-        epkgs.rainbow-delimiters
-        epkgs.rainbow-mode
-        epkgs.spaceline
-        epkgs.treemacs-icons-dired
-        epkgs.treemacs-magit
-        epkgs.treemacs-projectile
-        epkgs.underwater-theme
-        epkgs.use-package
-        epkgs.yasnippet-snippets
-      ];
+      # extraEmacsPackages = epkgs: [
+      #   epkgs.auto-compile
+      #   epkgs.blacken
+      #   epkgs.company
+      #   epkgs.dashboard
+      #   epkgs.expand-region
+      #   epkgs.flycheck
+      #   epkgs.latex-preview-pane
+      #   epkgs.lsp-jedi
+      #   epkgs.lsp-ui
+      #   epkgs.minions
+      #   epkgs.nix-mode
+      #   epkgs.nix-modeline
+      #   epkgs.org-books
+      #   epkgs.org-chef
+      #   epkgs.popup-kill-ring
+      #   epkgs.rainbow-delimiters
+      #   epkgs.rainbow-mode
+      #   epkgs.spaceline
+      #   epkgs.treemacs-icons-dired
+      #   epkgs.treemacs-magit
+      #   epkgs.treemacs-projectile
+      #   epkgs.underwater-theme
+      #   epkgs.use-package
+      #   epkgs.yasnippet-snippets
+      # ];
     })
   ];
 }
