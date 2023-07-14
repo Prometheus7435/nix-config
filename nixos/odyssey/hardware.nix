7d494c9e7632fdb23219ab963cafa76f3dda3dae
Zach Bombay <zacharybombay@gmail.com>
2023-07-14 18:10:51 -0400

    uncomment use open source nvidia drivers

--- nixos/odyssey/hardware.nix
+++ nixos/odyssey/hardware.nix
@@ -145,29 +145,28 @@
       "nvidia-settings"
     ];
 
-  # # Tell Xorg to use the nvidia driver
-  # services.xserver.videoDrivers = ["nvidia"];
+  # Tell Xorg to use the nvidia driver
+  services.xserver.videoDrivers = ["nvidia"];
 
-  # hardware.nvidia = {
+  hardware.nvidia = {
 
-  #   # Modesetting is needed for most wayland compositors
-  #   modesetting.enable = true;
-
-
-  #   prime = {
-  #     nvidiaBusId = "PCI:45:0:0";
-  #     # nvidiaBusId = "PCI:1:0:0";
-  #   };
+    # Modesetting is needed for most wayland compositors
+    modesetting.enable = true;
 
 
-  #   # Use the open source version of the kernel module
-  #   # Only available on driver 515.43.04+
-  #   # open = true;
+    prime = {
+      nvidiaBusId = "PCI:45:0:0";
+    };
 
-  #   # Enable the nvidia settings menu
-  #   nvidiaSettings = true;
 
-  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  #   # package = config.boot.kernelPackages.nvidiaPackages.stable;
-  # };
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    open = true;
+
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
