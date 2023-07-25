201b504244f83038eb1b21c3e777966c10a64cc2
Zach Bombay <zacharybombay@gmail.com>
2023-07-24 23:13:30 -0400

    different config for emacs-config.nix

--- nixos/_mixins/base/default.nix
+++ nixos/_mixins/base/default.nix
@@ -3,7 +3,7 @@
   imports = [
     ./locale.nix
     ./nano.nix
-    # ./emacs-config.nix
+    ./emacs-config.nix
     ../services/fwupd.nix
     ../services/openssh.nix
     ../services/tailscale.nix
--- nixos/_mixins/base/emacs-config.nix
+++ nixos/_mixins/base/emacs-config.nix
@@ -4,55 +4,68 @@
   imports = [
 
   ];
+
   services.emacs = {
     enable = true;
-    package = pkgs.emacs-unstable;
+    install = true;
     defaultEditor = true;
+    package = with pkgs; (emacsWithPackagesFromUsePackage
+      {
+      config = ./emacs-config.org;
+      package = pkgs.emacsGit;
+      alwaysEnsure = false;
+      }
+    );
   };
+  # services.emacs = {
+  #   enable = true;
+  #   package = pkgs.emacs-unstable;
+  #   defaultEditor = true;
+  # };
 
-  environment.systemPackages = [
-    # applications needed for the emacs packages to hook into
-    pkgs.python311Full
-    pkgs.python311Packages.jedi
-    pkgs.python311Packages.black
-    pkgs.python311Packages.jedi-language-server
-    pkgs.texlive.combined.scheme-full
+  # environment.systemPackages = with pkgs; [
+  #   # applications needed for the emacs packages to hook into
+  #   python311Full
+  #   python311Packages.jedi
+  #   python311Packages.black
+  #   python311Packages.jedi-language-server
+  #   texlive.combined.scheme-full
 
+  # # ];
+  #   (emacsWithPackagesFromUsePackage {
+  #     # package = pkgs.emacs-unstable;  # replace with pkgs.emacsPgtk, or another version if desired.
+  #     # config = /home/zach/sync/dot_files/dot_emacs.d/init.el;
+  #     config = ./emacs-config.org; # Org-Babel configs also supported
+  #     alwaysEnsure = true;
+  #     alwaysTangle = true;
+  #     defaultInitFile = true;
+
+  #     # extraEmacsPackages = epkgs: [
+  #     #   epkgs.auto-compile
+  #     #   epkgs.blacken
+  #     #   epkgs.company
+  #     #   epkgs.dashboard
+  #     #   epkgs.expand-region
+  #     #   epkgs.flycheck
+  #     #   epkgs.latex-preview-pane
+  #     #   epkgs.lsp-jedi
+  #     #   epkgs.lsp-ui
+  #     #   epkgs.minions
+  #     #   epkgs.nix-mode
+  #     #   epkgs.nix-modeline
+  #     #   epkgs.org-books
+  #     #   epkgs.org-chef
+  #     #   epkgs.popup-kill-ring
+  #     #   epkgs.rainbow-delimiters
+  #     #   epkgs.rainbow-mode
+  #     #   epkgs.spaceline
+  #     #   epkgs.treemacs-icons-dired
+  #     #   epkgs.treemacs-magit
+  #     #   epkgs.treemacs-projectile
+  #     #   epkgs.underwater-theme
+  #     #   epkgs.use-package
+  #     #   epkgs.yasnippet-snippets
+  #     # ];
+  #   })
   # ];
-    (pkgs.emacsWithPackagesFromUsePackage {
-      package = pkgs.emacs-unstable;  # replace with pkgs.emacsPgtk, or another version if desired.
-      # config = /home/zach/sync/dot_files/dot_emacs.d/init.el;
-      config = ./emacs-config.org; # Org-Babel configs also supported
-      alwaysEnsure = true;
-      alwaysTangle = true;
-      defaultInitFile = true;
-
-      # extraEmacsPackages = epkgs: [
-      #   epkgs.auto-compile
-      #   epkgs.blacken
-      #   epkgs.company
-      #   epkgs.dashboard
-      #   epkgs.expand-region
-      #   epkgs.flycheck
-      #   epkgs.latex-preview-pane
-      #   epkgs.lsp-jedi
-      #   epkgs.lsp-ui
-      #   epkgs.minions
-      #   epkgs.nix-mode
-      #   epkgs.nix-modeline
-      #   epkgs.org-books
-      #   epkgs.org-chef
-      #   epkgs.popup-kill-ring
-      #   epkgs.rainbow-delimiters
-      #   epkgs.rainbow-mode
-      #   epkgs.spaceline
-      #   epkgs.treemacs-icons-dired
-      #   epkgs.treemacs-magit
-      #   epkgs.treemacs-projectile
-      #   epkgs.underwater-theme
-      #   epkgs.use-package
-      #   epkgs.yasnippet-snippets
-      # ];
-    })
-  ];
 }
