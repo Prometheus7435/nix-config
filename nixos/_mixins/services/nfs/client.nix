43f4b8e6ebca0265bfc19c11f137e1ae64ad982c
Zach Bombay <zacharybombay@gmail.com>
2023-07-29 12:32:19 -0400

    remove user requirement in nfs client.nix

--- nixos/_mixins/services/nfs/client.nix
+++ nixos/_mixins/services/nfs/client.nix
@@ -1,4 +1,4 @@
-{config, ...}:
+{config, username, ...}:
 let
   nfs_folder = "";
 
@@ -6,13 +6,14 @@ in
 {
   # NFS drives
   ## sg1
-  fileSystems."/mnt/nfs/${user}" = {
+  fileSystems."/mnt/nfs/zach" = {
     device = "10.10.10.11:/atlantis/nfs/zach";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
   };
 
   ## starbase
+##   fileSystems."/mnt/nfs/${user}" = {  ## for future
   fileSystems."/mnt/zach" = {
     device = "starbase.tail0301a.ts.net:/mnt/alpha/nfs/zach";
     # device = "10.10.10.12:/mnt/alpha/nfs/zach";
