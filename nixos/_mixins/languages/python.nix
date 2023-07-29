00f942fdfe886c810b7e9dd2b41ad2b710266298
Zach Bombay <zacharybombay@gmail.com>
2023-07-29 12:43:23 -0400

    correct syntax error in python.nix

--- nixos/_mixins/languages/python.nix
+++ nixos/_mixins/languages/python.nix
@@ -1,17 +1,17 @@
-{lib, pkgs, ...}: {
-  let
-    my-python-packages = ps: with ps; [
-      pandas
-      requests
-      jedi
-      black
-      jedi-language-server
-      # other python packages
-    ];
-  in
-    environment.systemPackages = with pkgs; [
-      python3Full
-      (python3Full.withPackages my-python-packages)
-    ];
-
+{lib, pkgs, ...}:
+let
+  my-python-packages = ps: with ps; [
+    pandas
+    requests
+    jedi
+    black
+    jedi-language-server
+    # other python packages
+  ];
+in
+{
+  environment.systemPackages = with pkgs; [
+    python3Full
+    (python3Full.withPackages my-python-packages)
+  ];
 }
