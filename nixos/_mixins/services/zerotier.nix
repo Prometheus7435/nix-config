0d63c12966f6f3876f88c238289de003c3a032ae
Zachary Bombay <zacharybombay@gmail.com>
2023-07-30 13:20:08 -0400

    changed firewall to allow zerotier port

--- nixos/_mixins/services/zerotier.nix
+++ nixos/_mixins/services/zerotier.nix
@@ -1,4 +1,12 @@
 { config, ... }: {
+  networking = {
+    firewall = {
+      checkReversePath = "loose";
+      allowedUDPPorts = [ config.services.zerotierone.port ];
+      # trustedInterfaces = [
+      # ];
+    };
+  };
   services.zerotierone = {
     enable = true;
     joinNetworks = [ "" ];
