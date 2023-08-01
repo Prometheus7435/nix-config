2ced17ec73e9eb76a4512c0511f96663870e4932
Zach Bombay <zacharybombay@gmail.com>
2023-07-31 21:09:23 -0400

    change akira tlp to charge to 95

--- nixos/akira/hardware.nix
+++ nixos/akira/hardware.nix
@@ -68,7 +68,7 @@
     tlp = {
       settings = {
         START_CHARGE_THRESH_BAT0 = "75";
-        STOP_CHARGE_THRESH_BAT0 = "80";
+        STOP_CHARGE_THRESH_BAT0 = "95";
       };
     };
     # fingerprint reader
