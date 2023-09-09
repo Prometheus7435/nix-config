a4c88bd8049431ffc5cff7e28564a6574df9ce44
Zachary Bombay <zacharybombay@gmail.com>
2023-09-08 22:49:29 -0400

    create and merge default.nix for akira

---
+++ nixos/akira/default.nix
@@ -0,0 +1,118 @@
+# ThinkPad X1 Tablet Gen 3
+# Motherboard:
+# CPU: Intel i7-8650U (8)
+# GPU:
+# RAM: 16GB
+# NVME: 500Gb
+
+{ config, lib, pkgs, modulesPath, ... }: {
+  imports = [
+    inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x13
+    inputs.nixos-hardware.nixosModules.common-cpu-intel
+    inputs.nixos-hardware.nixosModules.common-gpu-intel
+    inputs.nixos-hardware.nixosModules.common-pc
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
+    (modulesPath + "/installer/scan/not-detected.nix")
+    ../_mixins/hardware/mobile.nix
+    ../_mixins/services/pipewire.nix
+    ../_mixins/hardware/network-dhcp.nix
+    ../_mixins/hardware/zfs.nix
+    ../_mixins/hardware/default.nix
+  ];
+
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        devices = [ "nodev" ];
+        efiInstallAsRemovable = true;
+        efiSupport = true;
+        useOSProber = true;
+        configurationLimit = 8;
+        # splashImage = ./bonsai.png;
+      };
+      timeout = 3;
+    };
+
+    kernelParams = [ "mem_sleep_default=deep" ];
+    extraModulePackages = with config.boot.kernelPackages; [
+      acpi_call
+    ];
+
+    initrd = {
+      availableKernelModules = [
+
+      ];
+      kernelModules = [
+
+      ];
+    };
+  };
+
+  services = {
+    zfs.autoScrub.enable = true;
+    fstrim.enable = true;
+  };
+
+
+
+  swapDevices = [ ];
+
+   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  # networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  environment.systemPackages = with pkgs; [
+
+  ];
+
+  hardware = {
+    # bluetooth.enable = true;
+    # bluetooth.settings = {
+    #   General = {
+    #     Enable = "Source,Sink,Media,Socket";
+    #   };
+    # };
+    # opengl = {
+    #   enable = true;
+    #   extraPackages = with pkgs; [
+    #     intel-media-driver # LIBVA_DRIVER_NAME=iHD
+    #     vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for FF/Chromium)
+    #     vaapiVdpau
+    #     libvdpau-va-gl
+    #     intel-ocl
+    #     rocm-opencl-icd
+    #   ];
+    #   driSupport = true;
+    #   driSupport32Bit = true;
+    # };
+    # sensor = {
+    #   # automatic screen orientation
+    #   iio = {
+    #     enable = true;
+    #   };
+    # };
+  };
+  # Enable touchpad support (enabled default in most desktopManager).
+  services = {
+    xserver.libinput.enable = true;
+    tlp = {
+      settings = {
+        START_CHARGE_THRESH_BAT0 = "75";
+        STOP_CHARGE_THRESH_BAT0 = "95";
+      };
+    };
+    # fingerprint reader
+    fprintd.enable = true;
+
+  };
+  security = {
+    pam.services.login.fprintAuth = true;
+    pam.services.xscreensaver.fprintAuth = true;
+  };
+}
