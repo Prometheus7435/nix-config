71d59cbd08023fb1943b4ca412d45e4e2a9dfacb
Zach Bombay <zacharybombay@gmail.com>
2023-09-12 18:42:24 -0400

    add docker-compose app to install

--- nixos/_mixins/containers/default.nix
+++ nixos/_mixins/containers/default.nix
@@ -8,6 +8,9 @@
   ];
 
   virtualisation.docker.enable = true;
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
   # virtualisation.docker.rootless = {
   #   enable = true;
   #   setSocketVariable = true;
