ce5150cb6875d7276764058b3eb20ac69479f18a
Zach Bombay <zacharybombay@gmail.com>
2023-09-21 20:39:27 -0400

    reconfig to try if this config will work

--- nixos/cali/default.nix
+++ nixos/cali/default.nix
@@ -6,14 +6,13 @@
 { config, inputs, lib, pkgs, username, modulesPath, ... }:
 {
   imports = [
-    # ./disks.nix
-    # inputs.disko.nixosModules.disko
+    (modulesPath + "/profiles/qemu-guest.nix")
+    # (import ./disks.nix { })
     inputs.nixos-hardware.nixosModules.common-cpu-amd
     inputs.nixos-hardware.nixosModules.common-gpu-amd
-    # inputs.nixos-hardware.nixosModules.common-gpu-intel
     inputs.nixos-hardware.nixosModules.common-pc
     inputs.nixos-hardware.nixosModules.common-pc-ssd
-    (modulesPath + "/installer/scan/not-detected.nix")
+    # (modulesPath + "/installer/scan/not-detected.nix")
     ../_mixins/services/pipewire.nix
     ../_mixins/hardware/network-dhcp.nix
     ../_mixins/hardware/default.nix
@@ -24,12 +23,19 @@
   boot = {
     kernelPackages = pkgs.linuxPackages;
     extraModulePackages = with config.boot.kernelPackages; [
-      acpi_call
+
     ];
 
     initrd = {
       availableKernelModules = [
-
+        "xhci_pci"
+        "ohci_pci"
+        "ehci_pci"
+        "virtio_pci"
+        "ahci"
+        "usbhid"
+        "sr_mod"
+        "virtio_blk"
       ];
       kernelModules = [
 
--- nixos/cali/disks.nix
+++ nixos/cali/disks.nix
@@ -1,42 +1,94 @@
 # enp132s0f0
+
+## Straight copy from Whimpy's vm config
+{ disks ? [ "/dev/vda" ], ... }:
+let
+  defaultXfsOpts = [ "defaults" "relatime" "nodiratime" ];
+in
 {
   disko.devices = {
     disk = {
       vda = {
-        device = "/dev/vda";
         type = "disk";
+        device = builtins.elemAt disks 0;
         content = {
-          type = "gpt";
-          partitions = {
-            # name = "ESP";
-            # start = "0";
-            # end = "512MiB";  # way overkill because I'm tired of getting errors because I ran out of space
-            # fs-type = "fat32";
-            # bootable = true;
-            # content = {
-            #   type = "filesystem";
-            #   format = "vfat";
-            #   mountpoint = "/boot";
-            ESP = {
-              type = "EF00";
-              size = "500M";
+          type = "table";
+          format = "gpt";
+          partitions = [{
+            name = "boot";
+            start = "0%";
+            end = "1M";
+            flags = [ "bios_grub" ];
+          }
+            {
+              name = "ESP";
+              start = "1M";
+              end = "550MiB";
+              bootable = true;
+              flags = [ "esp" ];
+              fs-type = "fat32";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
               };
-            };
-            root = {
-              size = "100%";
+            }
+            {
+              name = "root";
+              start = "560MiB";
+              end = "100%";
               content = {
                 type = "filesystem";
-                format = "ext4";
+                # Overwirte the existing filesystem
+                extraArgs = [ "-f" ];
+                format = "xfs";
                 mountpoint = "/";
+                mountOptions = defaultXfsOpts;
               };
-            };
-          };
+            }];
         };
       };
     };
   };
 }
+# {
+#   disko.devices = {
+#     disk = {
+#       vda = {
+#         device = "/dev/vda";
+#         type = "disk";
+#         content = {
+#           type = "gpt";
+#           partitions = {
+#             # name = "ESP";
+#             # start = "0";
+#             # end = "512MiB";  # way overkill because I'm tired of getting errors because I ran out of space
+#             # fs-type = "fat32";
+#             # bootable = true;
+#             # content = {
+#             #   type = "filesystem";
+#             #   format = "vfat";
+#             #   mountpoint = "/boot";
+#             ESP = {
+#               type = "EF00";
+#               size = "500M";
+#               content = {
+#                 type = "filesystem";
+#                 format = "vfat";
+#                 mountpoint = "/boot";
+#               };
+#             };
+#             root = {
+#               size = "100%";
+#               content = {
+#                 type = "filesystem";
+#                 format = "ext4";
+#                 mountpoint = "/";
+#               };
+#             };
+#           };
+#         };
+#       };
+#     };
+#   };
+# }
