3aca3971230769a9283c446dd75c98306c8e1f87
Zach Bombay <zacharybombay@gmail.com>
2023-10-13 10:42:11 -0400

    forgot the ;

--- nixos/_mixins/hardware/zfs.nix
+++ nixos/_mixins/hardware/zfs.nix
@@ -5,7 +5,7 @@
   boot = {
     supportedFilesystems = [ "zfs" ];
     zfs.requestEncryptionCredentials = true;
-    kernelPackages = pkgs.linuxPackages_xanmod_stable
+    kernelPackages = pkgs.linuxPackages_xanmod_stable;
     # kernelPackages = pkgs.linuxKernel.packages.linux_xanmod.zfs
     # kernelPackages = pkgs.linuxPackages_lqx; # zen like kernel
   };
