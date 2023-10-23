9ba172b4bf4656243c33e0ce64be3ad17cd3dfd0
Zach Bombay <zacharybombay@gmail.com>
2023-10-23 11:42:34 -0400

    comment out others config and add fish integration

--- home-manager/console/starship.nix
+++ home-manager/console/starship.nix
@@ -1,24 +1,27 @@
 # {config, pkgs, ...} {
 {
-  programs.starship.enable = true;
-  programs.starship.settings = {
-    add_newline = false;
-    format = "$shlvl$shell$username$hostname$nix_shell$git_branch$git_commit$git_state$git_status$directory$jobs$cmd_duration$character";
-    shlvl = {
-      disabled = false;
-      symbol = "ﰬ";
-      style = "bright-red bold";
-    };
-    shell = {
-      disabled = false;
-      format = "$indicator";
-      fish_indicator = "";
-      bash_indicator = "[BASH](bright-white) ";
-      zsh_indicator = "[ZSH](bright-white) ";
-    };
-    username = {
-      style_user = "bright-white bold";
-      style_root = "bright-red bold";
-    };
-  }
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    # settings = {
+    #   add_newline = false;
+    #   format = "$shlvl$shell$username$hostname$nix_shell$git_branch$git_commit$git_state$git_status$directory$jobs$cmd_duration$character";
+    #   shlvl = {
+    #     disabled = false;
+    #     symbol = "ﰬ";
+    #     style = "bright-red bold";
+    #   };
+    #   shell = {
+    #     disabled = false;
+    #     format = "$indicator";
+    #     fish_indicator = "";
+    #     bash_indicator = "[BASH](bright-white) ";
+    #     zsh_indicator = "[ZSH](bright-white) ";
+    #   };
+    #   username = {
+    #     style_user = "bright-white bold";
+    #     style_root = "bright-red bold";
+    #   };
+    # };
+  };
 }
