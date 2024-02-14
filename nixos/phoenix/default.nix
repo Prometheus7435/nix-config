e9fdbcf11658354bda42177985474f1074d64db1
Zach Bombay <zacharybombay@gmail.com>
2024-02-14 03:36:34 -0500

    create openssl module with architecture specifics

--- nixos/phoenix/default.nix
+++ nixos/phoenix/default.nix
@@ -36,7 +36,7 @@
   boot = {
     supportedFilesystems = [ "zfs" ];
     zfs.requestEncryptionCredentials = true;
-    kernelPackages = pkgs.linuxPackages_latest; # lqx is less frequent release version of zen
+    kernelPackages = pkgs.linuxPackages_lqx; # lqx is less frequent release version of zen
     # kernelPackages = pkgs.linuxPackages_xanmod_latest;
     # supportedFilesystems = [ "ntfs" "xfs" "ext4" ];
     kernelParams = [ "nohibernate"];
