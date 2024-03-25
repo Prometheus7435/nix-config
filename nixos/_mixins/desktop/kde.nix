4887f47407495677d9ee63692d7389c44b22fbd2
Zach Bombay <zacharybombay@gmail.com>
2024-03-25 14:46:56 -0400

    forgot to move back for packages

--- nixos/_mixins/desktop/kde.nix
+++ nixos/_mixins/desktop/kde.nix
@@ -51,29 +51,29 @@
 
   # nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
-    kdePackages.applet-window-buttons
-    kdePackages.discover
-    kdePackages.dragon
-    kdePackages.filelight
-    kdePackages.kalk
-    kdePackages.kdeconnect-kde
-    kdePackages.kfind
-    kdePackages.plasma-integration
-    kdePackages.xdg-desktop-portal-kde
-    kdePackages.kdecoration
-    kdePackages.qtstyleplugin-kvantum
+    # kdePackages.applet-window-buttons
+    # kdePackages.discover
+    # kdePackages.dragon
+    # kdePackages.filelight
+    # kdePackages.kalk
+    # kdePackages.kdeconnect-kde
+    # kdePackages.kfind
+    # kdePackages.plasma-integration
+    # kdePackages.xdg-desktop-portal-kde
+    # kdePackages.kdecoration
+    # kdePackages.qtstyleplugin-kvantum
 
-    # libsForQt5.applet-window-buttons
-    # libsForQt5.discover
-    # libsForQt5.dragon
-    # libsForQt5.filelight
-    # libsForQt5.kalk
-    # libsForQt5.kdeconnect-kde
-    # libsForQt5.kfind
-    # libsForQt5.plasma-integration
-    # libsForQt5.xdg-desktop-portal-kde
-    # libsForQt5.kdecoration
-    # libsForQt5.qtstyleplugin-kvantum
+    libsForQt5.applet-window-buttons
+    libsForQt5.discover
+    libsForQt5.dragon
+    libsForQt5.filelight
+    libsForQt5.kalk
+    libsForQt5.kdeconnect-kde
+    libsForQt5.kfind
+    libsForQt5.plasma-integration
+    libsForQt5.xdg-desktop-portal-kde
+    libsForQt5.kdecoration
+    libsForQt5.qtstyleplugin-kvantum
 
     okular
     yakuake
