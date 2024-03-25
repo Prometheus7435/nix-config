19095e8056f325df140a19d8b7a4739080b92347
Zach Bombay <zacharybombay@gmail.com>
2024-03-25 14:33:41 -0400

    update to plasma 6

--- nixos/_mixins/desktop/kde.nix
+++ nixos/_mixins/desktop/kde.nix
@@ -12,13 +12,17 @@
       xkb.options = "grp:win_space_toggle";
 
       desktopManager = {
-        plasma5 = {
+        plasma6 = {
+        # plasma5 = {
           enable = true;
         };
       };
 
       displayManager = {
-        # sddm.enable = true;
+        # sddm = {
+        #   enable = true;
+        #   wayland.enable = true;
+        # };
         lightdm.enable = true;  # lets me autoLogin
         defaultSession = "plasmawayland";
         autoLogin = {
@@ -55,17 +59,30 @@
 
   # nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
-    libsForQt5.applet-window-buttons
-    libsForQt5.discover
-    libsForQt5.dragon
-    libsForQt5.filelight
-    libsForQt5.kalk
-    libsForQt5.kdeconnect-kde
-    libsForQt5.kfind
-    libsForQt5.plasma-integration
-    libsForQt5.xdg-desktop-portal-kde
-    libsForQt5.kdecoration
-    libsForQt5.qtstyleplugin-kvantum
+    kdePackages.applet-window-buttons
+    kdePackages.discover
+    kdePackages.dragon
+    kdePackages.filelight
+    kdePackages.kalk
+    kdePackages.kdeconnect-kde
+    kdePackages.kfind
+    kdePackages.plasma-integration
+    kdePackages.xdg-desktop-portal-kde
+    kdePackages.kdecoration
+    kdePackages.qtstyleplugin-kvantum
+
+    # libsForQt5.applet-window-buttons
+    # libsForQt5.discover
+    # libsForQt5.dragon
+    # libsForQt5.filelight
+    # libsForQt5.kalk
+    # libsForQt5.kdeconnect-kde
+    # libsForQt5.kfind
+    # libsForQt5.plasma-integration
+    # libsForQt5.xdg-desktop-portal-kde
+    # libsForQt5.kdecoration
+    # libsForQt5.qtstyleplugin-kvantum
+
     okular
     yakuake
     kate
