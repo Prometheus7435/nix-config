59d8e83635746c302b6b865bba85f0380b40a3ad
Zach Bombay <zacharybombay@gmail.com>
2024-03-25 14:44:56 -0400

    uncomment hostPlatform

--- nixos/phoenix/default.nix
+++ nixos/phoenix/default.nix
@@ -42,7 +42,7 @@
 
   swapDevices = [ ];
 
-  # nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   # hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   boot = {
