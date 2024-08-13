67521420414775aea536ffb82f0076dcb8404fcb
Zach Bombay <zacharybombay@gmail.com>
2024-08-13 13:37:22 -0400

    update stateVersion to 24.05

--- flake.nix
+++ flake.nix
@@ -75,7 +75,7 @@
 
       # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
       # stateVersion = "unstable";
-      stateVersion = "23.11";
+      stateVersion = "24.05";
 
     in rec {
       # Your custom packages
