19c10b440d905272f3b9c95e29536f87bd83a0de
Zach Bombay <zacharybombay@gmail.com>
2024-08-21 22:06:35 -0400

    add back in default and virtualization in boxes

---
+++ nixos/_mixins/boxes/virtualization.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, callPackage, ... }: {
+  environment.systemPackages = with pkgs; [
+    guestfs-tools
+    libguestfs
+    libguestfs-appliance
+    libguestfs-with-appliance
+    libvirt
+    # qemu_full
+    # qemu-utils
+    # quickemu
+    spice-gtk
+    virt-manager
+    virt-viewer
+    virtiofsd
+  ];
+  virtualisation.libvirtd.enable = true;
+
+}
