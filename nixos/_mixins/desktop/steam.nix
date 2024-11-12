0f7be1a08ada8bc620601f975b6587a9812e0295
Zach Bombay <zacharybombay@gmail.com>
2024-11-11 21:41:42 -0500

    comment out fontconfig as it gave errors

--- nixos/_mixins/desktop/steam.nix
+++ nixos/_mixins/desktop/steam.nix
@@ -1,6 +1,6 @@
 { ... }: {
   # https://nixos.wiki/wiki/Steam
-  fontconfig.cache32Bit = true;
+  # fontconfig.cache32Bit = true;
   hardware.steam-hardware.enable = true;
   opengl.driSupport32Bit = true;
   programs = {
