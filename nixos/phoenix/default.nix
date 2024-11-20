bd3ceb19a475598b4b2c28221248d58be80eeec6
Zach Bombay <zacharybombay@gmail.com>
2024-11-20 17:24:53 -0500

    changed kernel to 6 12

--- nixos/phoenix/default.nix
+++ nixos/phoenix/default.nix
@@ -56,8 +56,8 @@
   boot = {
     supportedFilesystems = [ "zfs" ];
     zfs.requestEncryptionCredentials = true;
-    # kernelPackages = pkgs.linuxPackages_6_11;
-    kernelPackages = pkgs.linuxKernel.packages.linux_xanmod;
+    kernelPackages = pkgs.linuxPackages_6_12;
+    # kernelPackages = pkgs.linuxKernel.packages.linux_xanmod;
     # kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
     kernelParams = [ "nohibernate"];
     kernelModules = [
