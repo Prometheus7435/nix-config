42bee5a21c857a594d7800d5270b25031135b5f4
Zach Bombay <zacharybombay@gmail.com>
2025-05-04 18:56:05 -0400

    put settings in the proper place

--- nixos/_mixins/services/nfs/server.nix
+++ nixos/_mixins/services/nfs/server.nix
@@ -1,23 +1,26 @@
 {config, ...}:
 
 {
-  services.nfs.server = {
-    enable = true;
-    exports = ''
-      ## Desktop - Windows
-        ## 100.109.81.2
-        ## 10.107.0.10
-      /mnt/alpha/iso_holder/media     10.107.0.10(rw,nohide,sync,no_subtree_check)
-      /mnt/alpha/media/tv             10.107.0.10(rw,nohide,sync,no_subtree_check)
-      /mnt/alpha/media/movies         10.107.0.10(rw,nohide,sync,no_subtree_check)
-      /mnt/alpha/nfs/zach             10.107.0.10(rw,nohide,sync,no_subtree_check)
+  services.nfs = {
+    server = {
+      enable = true;
+      exports = ''
+            ## Desktop - Windows
+            ## 100.109.81.2
+            ## 10.107.0.10
+           /mnt/alpha/iso_holder/media     10.107.0.10(rw,nohide,sync,no_subtree_check)
+           /mnt/alpha/media/tv             10.107.0.10(rw,nohide,sync,no_subtree_check)
+           /mnt/alpha/media/movies         10.107.0.10(rw,nohide,sync,no_subtree_check)
+           /mnt/alpha/nfs/zach             10.107.0.10(rw,nohide,sync,no_subtree_check)
 
-      ## Phoenix
-      /mnt/alpha/nfs/zach             100.123.61.88(rw,nohide,sync,no_subtree_check)
+           ## Phoenix
+           /mnt/alpha/nfs/zach             100.123.61.88(rw,nohide,sync,no_subtree_check)
 
-      ## Odyssey
-      /mnt/alpha/nfs/zach             100.78.103.103(rw,nohide,sync,no_subtree_check)
+           ## Odyssey
+           /mnt/alpha/nfs/zach             100.78.103.103(rw,nohide,sync,no_subtree_check)
           '';
+    };
+
     settings = {
       nfsd.udp = false;
       nfsd.vers3 = false;
