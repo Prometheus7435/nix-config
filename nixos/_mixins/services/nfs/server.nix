489fbe1a54b382cc345f8bf699354bc5797441d8
Zach Bombay <zacharybombay@gmail.com>
2025-06-04 18:04:33 -0400

    change ip for windows pc

--- nixos/_mixins/services/nfs/server.nix
+++ nixos/_mixins/services/nfs/server.nix
@@ -8,10 +8,10 @@
             ## Desktop - Windows
             ## 100.109.81.2
             ## 10.107.0.10
-           /mnt/alpha/iso_holder/media     10.107.0.10(rw,nohide,sync,no_subtree_check)
-           /mnt/alpha/media/tv             10.107.0.10(rw,nohide,sync,no_subtree_check)
-           /mnt/alpha/media/movies         10.107.0.10(rw,nohide,sync,no_subtree_check)
-           /mnt/alpha/nfs/zach             10.107.0.10(rw,nohide,sync,no_subtree_check)
+           /mnt/alpha/iso_holder/media     100.109.81.2(rw,nohide,sync,no_subtree_check)
+           /mnt/alpha/media/tv             100.109.81.2(rw,nohide,sync,no_subtree_check)
+           /mnt/alpha/media/movies         100.109.81.2(rw,nohide,sync,no_subtree_check)
+           /mnt/alpha/nfs/zach             100.109.81.2(rw,nohide,sync,no_subtree_check)
 
            ## Phoenix
            /mnt/alpha/nfs/zach             100.123.61.88(rw,nohide,sync,no_subtree_check)
