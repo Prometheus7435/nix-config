0612c8e55fb8b78e3877972ceb0b1879c11a16cf
Zach Bombay <zacharybombay@gmail.com>
2025-10-24 17:12:15 -0400

    remove nfs version and add serverIP variable

--- nixos/_mixins/services/nfs/client.nix
+++ nixos/_mixins/services/nfs/client.nix
@@ -1,7 +1,7 @@
 {config, username, ...}:
 let
   nfs_folder = "";
-
+  serverIP = "100.126.182.65"
 in
 {
   # NFS drives
@@ -15,15 +15,15 @@ in
   ## from starbase
 ##   fileSystems."/mnt/nfs/${username}" = {  ## for future
   fileSystems."/mnt/zach" = {
-    device = "100.126.182.65:/mnt/alpha/nfs/zach";
+    device = "${serverIP}:/mnt/alpha/nfs/zach";
     # device = "starbase.tail0301a.ts.net:/mnt/alpha/nfs/zach";
     # device = "10.10.10.12:/mnt/alpha/nfs/${username}";    # future config
     fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "user" "nfsvers=4.2"];
+    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "user"];
   };
   fileSystems."/mnt/media" = {
-    device = "100.126.182.65:/mnt/alpha/media";
+    device = "${serverIP}:/mnt/alpha/media";
     fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "user" "nfsvers=4.2"];
+    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "user"];
   };
 }
